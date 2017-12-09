60fe504e7f8b3983a2f853e8a076af6ebede1bf2
Abdullah <Abdullah@Abdullah-Lap>
2017-12-09 19:41:10 +0300

    Page-shopping-cart php

--- page-shopping-cart.php
+++ page-shopping-cart.php
@@ -26,13 +26,13 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-        
+        <?php 
         session_start();
         include "dbconnect.php";
-        $total=0;
+        $sql="";
         if (isset($_SESSION["number_of_items"])) {
             $number_of_items = $_SESSION["number_of_items"];
-            $sql = "select * from product where ";
+            $sql = "select * from products where ";
             for ($i = 0; $i < $number_of_items - 1; $i++) {
                 $sql = $sql . " id = " . $_SESSION["shopping_cart"][$i] . " or ";
             }
@@ -40,23 +40,23 @@
 
 
             $result = $conn->query($sql);
+            $sql=$sql." ";
         }
-        $sql=$sql." ";
+        
         ?>
+        
         <!-- Navigation & Logo-->
         <div class="mainmenu-wrapper">
 	        <div class="container">
 	        	<div class="menuextras">
 					<div class="extras">
 						<ul>
-							<li class="shopping-cart-items"><i class="glyphicon glyphicon-shopping-cart icon-white"></i> <a href="page-shopping-cart.html"><b>3 items</b></a></li>
+							<li class="shopping-cart-items"><i class="glyphicon glyphicon-shopping-cart icon-white"></i> <a href="page-shopping-cart.php"><b>0 items</b></a></li>
 							<li>
 								<div class="dropdown choose-country">
-									<a class="#" data-toggle="dropdown" href="#"><img src="img/flags/gb.png" alt="Great Britain"> UK</a>
+									<a class="#" data-toggle="dropdown" href="#"><img src="img/flags/sa.png" alt="Saudi Arabia"> KSA</a>
 									<ul class="dropdown-menu" role="menu">
 										<li role="menuitem"><a href="#"><img src="img/flags/us.png" alt="United States"> US</a></li>
-										<li role="menuitem"><a href="#"><img src="img/flags/de.png" alt="Germany"> DE</a></li>
-										<li role="menuitem"><a href="#"><img src="img/flags/es.png" alt="Spain"> ES</a></li>
 									</ul>
 								</div>
 							</li>
@@ -66,89 +66,25 @@
 		        </div>
 		        <nav id="mainmenu" class="mainmenu">
 					<ul>
-						<li class="logo-wrapper"><a href="index.html"><img src="img/mPurpose-logo.png" alt="Multipurpose Twitter Bootstrap Template"></a></li>
-						<li>
+						<li class="logo-wrapper"><a href="index.php"><img src="img/psu_logo.png" alt="PSU"></a></li>
+						<li class="active">
 							<a href="index.html">Home</a>
 						</li>
 						<li>
-							<a href="features.html">Features</a>
-						</li>
-						<li class="has-submenu active">
-							<a href="#">Pages +</a>
-							<div class="mainmenu-submenu">
-								<div class="mainmenu-submenu-inner"> 
-									<div>
-										<h4>Homepage</h4>
-										<ul>
-											<li><a href="index.html">Homepage (Sample 1)</a></li>
-											<li><a href="page-homepage-sample.html">Homepage (Sample 2)</a></li>
-										</ul>
-										<h4>Services & Pricing</h4>
-										<ul>
-											<li><a href="page-services-1-column.html">Services/Features (Rows)</a></li>
-											<li><a href="page-services-3-columns.html">Services/Features (3 Columns)</a></li>
-											<li><a href="page-services-4-columns.html">Services/Features (4 Columns)</a></li>
-											<li><a href="page-pricing.html">Pricing Table</a></li>
-										</ul>
-										<h4>Team & Open Vacancies</h4>
-										<ul>
-											<li><a href="page-team.html">Our Team</a></li>
-											<li><a href="page-vacancies.html">Open Vacancies (List)</a></li>
-											<li><a href="page-job-details.html">Open Vacancies (Job Details)</a></li>
-										</ul>
-									</div>
-									<div>
-										<h4>Our Work (Portfolio)</h4>
-										<ul>
-											<li><a href="page-portfolio-2-columns-1.html">Portfolio (2 Columns, Option 1)</a></li>
-											<li><a href="page-portfolio-2-columns-2.html">Portfolio (2 Columns, Option 2)</a></li>
-											<li><a href="page-portfolio-3-columns-1.html">Portfolio (3 Columns, Option 1)</a></li>
-											<li><a href="page-portfolio-3-columns-2.html">Portfolio (3 Columns, Option 2)</a></li>
-											<li><a href="page-portfolio-item.html">Portfolio Item (Project) Description</a></li>
-										</ul>
-										<h4>General Pages</h4>
-										<ul>
-											<li><a href="page-about-us.html">About Us</a></li>
-											<li><a href="page-contact-us.html">Contact Us</a></li>
-											<li><a href="page-faq.html">Frequently Asked Questions</a></li>
-											<li><a href="page-testimonials-clients.html">Testimonials & Clients</a></li>
-											<li><a href="page-events.html">Events</a></li>
-											<li><a href="page-404.html">404 Page</a></li>
-											<li><a href="page-sitemap.html">Sitemap</a></li>
-											<li><a href="page-login.html">Login</a></li>
-											<li><a href="page-register.html">Register</a></li>
-											<li><a href="page-password-reset.html">Password Reset</a></li>
-											<li><a href="page-terms-privacy.html">Terms & Privacy</a></li>
-											<li><a href="page-coming-soon.html">Coming Soon</a></li>
-										</ul>
-									</div>
-									<div>
-										<h4>eShop</h4>
-										<ul>
-											<li><a href="page-products-3-columns.html">Products listing (3 Columns)</a></li>
-											<li><a href="page-products-4-columns.html">Products listing (4 Columns)</a></li>
-											<li><a href="page-products-slider.html">Products Slider</a></li>
-											<li><a href="page-product-details.html">Product Details</a></li>
-											<li><a href="page-shopping-cart.html">Shopping Cart</a></li>
-										</ul>
-										<h4>Blog</h4>
-										<ul>
-											<li><a href="page-blog-posts.html">Blog Posts (List)</a></li>
-											<li><a href="page-blog-post-right-sidebar.html">Blog Single Post (Right Sidebar)</a></li>
-											<li><a href="page-blog-post-left-sidebar.html">Blog Single Post (Left Sidebar)</a></li>
-											<li><a href="page-news.html">Latest & Featured News</a></li>
-										</ul>
-									</div>
-								</div><!-- /mainmenu-submenu-inner -->
-							</div><!-- /mainmenu-submenu -->
+							<a href="#">Buy</a>
 						</li>
 						<li>
-							<a href="credits.html">Credits</a>
+							<a href="#">Sell</a>
+						</li>
+						<li>
+							<a href="#">My Products</a>
 						</li>
 					</ul>
 				</nav>
 			</div>
 		</div>
+
+        
 
         <!-- Page Title -->
 		<div class="section section-breadcrumbs">
@@ -164,6 +100,7 @@
         <div class="section">
 	    	<div class="container">
 				<div class="row">
+                                    
 					<div class="col-md-12">
 						<!-- Action Buttons -->
 						<div class="pull-right">
@@ -176,66 +113,40 @@
 					<div class="col-md-12">
 						<!-- Shopping Cart Items -->
 						<table class="shopping-cart">
+                                                        <?php
+                                                        $total = 0;
+                                                        $quantity =0;
+                                                        $result = $conn->query($sql);
+                                                        if (isset($_SESSION["number_of_items"])) {
+                                                            while ($row = $result->fetch_assoc()) {
+                                                                $quantity = substr_count($sql, "id = ".$row["id"]." ");
+                                                                $total = $quantity * $row["price"] + $total;
+                                                                ?>
 							<!-- Shopping Cart Item -->
 							<tr>
 								<!-- Shopping Cart Item Image -->
-								<td class="image"><a href="page-product-details.html"><img src="img/product1.jpg" alt="Item Name"></a></td>
+								<td class="image"><a href="page-product-details.html"><img src="<?php echo $row["image_link"]; ?>" alt="Item Name"></a></td>
 								<!-- Shopping Cart Item Description & Features -->
 								<td>
-									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
-									<div class="feature color">
-										Color: <span class="color-white"></span>
-									</div>
-									<div class="feature">Size: <b>XXL</b></div>
+									<div class="cart-item-title"><a href="page-product-details.html"><?php echo $row["name"]; ?></a></div>
+									
 								</td>
 								<!-- Shopping Cart Item Quantity -->
 								<td class="quantity">
-									<input class="form-control input-sm input-micro" type="text" value="1">
+									<input class="form-control input-sm input-micro" type="text" value="<?php $quantity ?>">
 								</td>
 								<!-- Shopping Cart Item Price -->
-								<td class="price">$999.99</td>
+								<td class="price"><?php echo $row["price"]; ?></td>
 								<!-- Shopping Cart Item Actions -->
 								<td class="actions">
 									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
 									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
 								</td>
 							</tr>
+                                                         <?php }
+    } ?>
 							<!-- End Shopping Cart Item -->
-							<tr>
-								<td class="image"><a href="page-product-details.html"><img src="img/product2.jpg" alt="Item Name"></a></td>
-								<td>
-									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
-									<div class="feature color">
-										Color: <span class="color-orange"></span>
-									</div>
-									<div class="feature">Size: <b>XXL</b></div>
-								</td>
-								<td class="quantity">
-									<input class="form-control input-sm input-micro" type="text" value="1">
-								</td>
-								<td class="price">$999.99</td>
-								<td class="actions">
-									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
-									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
-								</td>
-							</tr>
-							<tr>
-								<td class="image"><a href="page-product-details.html"><img src="img/product3.jpg" alt="Item Name"></a></td>
-								<td>
-									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
-									<div class="feature color">
-									</div>
-									<div class="feature">Size: <b>XXL</b></div>
-								</td>
-								<td class="quantity">
-									<input class="form-control input-sm input-micro" type="text" value="1">
-								</td>
-								<td class="price">$999.99</td>
-								<td class="actions">
-									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
-									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
-								</td>
-							</tr>
+							
 						</table>
 						<!-- End Shopping Cart Items -->
 					</div>
@@ -296,33 +207,23 @@
 	    <div class="footer">
 	    	<div class="container">
 		    	<div class="row">
-		    		<div class="col-footer col-md-3 col-xs-6">
-		    			<h3>Our Latest Work</h3>
-		    			<div class="portfolio-item">
-							<div class="portfolio-image">
-								<a href="page-portfolio-item.html"><img src="img/portfolio6.jpg" alt="Project Name"></a>
-							</div>
-						</div>
-		    		</div>
+
 		    		<div class="col-footer col-md-3 col-xs-6">
 		    			<h3>Navigate</h3>
 		    			<ul class="no-list-style footer-navigate-section">
-		    				<li><a href="page-blog-posts.html">Blog</a></li>
-		    				<li><a href="page-portfolio-3-columns-2.html">Portfolio</a></li>
-		    				<li><a href="page-products-3-columns.html">eShop</a></li>
-		    				<li><a href="page-services-3-columns.html">Services</a></li>
-		    				<li><a href="page-pricing.html">Pricing</a></li>
-		    				<li><a href="page-faq.html">FAQ</a></li>
+		    				<li><a href="#">About Us</a></li>
+		    				<li><a href="#">Contact Us</a></li>
+		    				<li><a href="#">Services</a></li>
+		    				<li><a href="#">FAQ</a></li>
 		    			</ul>
 		    		</div>
 		    		
 		    		<div class="col-footer col-md-4 col-xs-6">
 		    			<h3>Contacts</h3>
 		    			<p class="contact-us-details">
-	        				<b>Address:</b> 123 Fake Street, LN1 2ST, London, United Kingdom<br/>
-	        				<b>Phone:</b> +44 123 654321<br/>
-	        				<b>Fax:</b> +44 123 654321<br/>
-	        				<b>Email:</b> <a href="mailto:getintoutch@yourcompanydomain.com">getintoutch@yourcompanydomain.com</a>
+	        				<b>Address:</b> Riyadh, Saudi Arabia<br/>
+	        				<b>Phone:</b> +966 55 2020770<br/>
+	        				<b>Email:</b> <a href="mailto:m3n991@gmail.com">m3n991@gmail.com</a>
 	        			</p>
 		    		</div>
 		    		<div class="col-footer col-md-2 col-xs-6">
@@ -336,7 +237,7 @@
 		    	</div>
 		    	<div class="row">
 		    		<div class="col-md-12">
-		    			<div class="footer-copyright">&copy; 2013 mPurpose. All rights reserved.</div>
+		    			<div class="footer-copyright">&copy; PSU Events, Web Project</div>
 		    		</div>
 		    	</div>
 		    </div>
