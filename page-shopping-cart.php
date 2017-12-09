3b952d455120872c7f59c80d608b0fc9066c3bee
Abdullah <Abdullah@Abdullah-Lap>
2017-12-09 18:59:58 +0300

    Shopping cart Page

--- page-shopping-cart.php
+++ page-shopping-cart.php
@@ -27,7 +27,22 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
         
+        session_start();
+        include "dbconnect.php";
+        $total=0;
+        if (isset($_SESSION["number_of_items"])) {
+            $number_of_items = $_SESSION["number_of_items"];
+            $sql = "select * from product where ";
+            for ($i = 0; $i < $number_of_items - 1; $i++) {
+                $sql = $sql . " id = " . $_SESSION["shopping_cart"][$i] . " or ";
+            }
+            $sql = $sql . " id = " . $_SESSION["shopping_cart"][$number_of_items - 1];
 
+
+            $result = $conn->query($sql);
+        }
+        $sql=$sql." ";
+        ?>
         <!-- Navigation & Logo-->
         <div class="mainmenu-wrapper">
 	        <div class="container">
@@ -140,7 +155,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-						<h1>Register</h1>
+						<h1>Shopping Cart</h1>
 					</div>
 				</div>
 			</div>
@@ -149,33 +164,128 @@
         <div class="section">
 	    	<div class="container">
 				<div class="row">
-					<div class="col-sm-5">
-						<div class="basic-login">
-							<form role="form">
-								<div class="form-group">
-		        				 	<label for="register-username"><i class="icon-user"></i> <b>Email</b></label>
-									<input class="form-control" id="register-username" type="text" placeholder="">
-								</div>
-								<div class="form-group">
-		        				 	<label for="register-password"><i class="icon-lock"></i> <b>Password</b></label>
-									<input class="form-control" id="register-password" type="password" placeholder="">
-								</div>
-								<div class="form-group">
-		        				 	<label for="register-password2"><i class="icon-lock"></i> <b>Re-enter Password</b></label>
-									<input class="form-control" id="register-password2" type="password" placeholder="">
-								</div>
-								<div class="form-group">
-									<button type="submit" class="btn pull-right">Register</button>
-									<div class="clearfix"></div>
-								</div>
-							</form>
+					<div class="col-md-12">
+						<!-- Action Buttons -->
+						<div class="pull-right">
+							<a href="#" class="btn btn-grey"><i class="glyphicon glyphicon-refresh"></i> UPDATE</a>
+							<a href="#" class="btn"><i class="glyphicon glyphicon-shopping-cart icon-white"></i> CHECK OUT</a>
 						</div>
 					</div>
-					<div class="col-sm-6 col-sm-offset-1 social-login">
-						<p>You can use your Facebook or Twitter for registration</p>
-						<div class="social-login-buttons">
-							<a href="#" class="btn-facebook-login">Use Facebook</a>
-							<a href="#" class="btn-twitter-login">Use Twitter</a>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<!-- Shopping Cart Items -->
+						<table class="shopping-cart">
+							<!-- Shopping Cart Item -->
+							<tr>
+								<!-- Shopping Cart Item Image -->
+								<td class="image"><a href="page-product-details.html"><img src="img/product1.jpg" alt="Item Name"></a></td>
+								<!-- Shopping Cart Item Description & Features -->
+								<td>
+									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
+									<div class="feature color">
+										Color: <span class="color-white"></span>
+									</div>
+									<div class="feature">Size: <b>XXL</b></div>
+								</td>
+								<!-- Shopping Cart Item Quantity -->
+								<td class="quantity">
+									<input class="form-control input-sm input-micro" type="text" value="1">
+								</td>
+								<!-- Shopping Cart Item Price -->
+								<td class="price">$999.99</td>
+								<!-- Shopping Cart Item Actions -->
+								<td class="actions">
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
+								</td>
+							</tr>
+							<!-- End Shopping Cart Item -->
+							<tr>
+								<td class="image"><a href="page-product-details.html"><img src="img/product2.jpg" alt="Item Name"></a></td>
+								<td>
+									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
+									<div class="feature color">
+										Color: <span class="color-orange"></span>
+									</div>
+									<div class="feature">Size: <b>XXL</b></div>
+								</td>
+								<td class="quantity">
+									<input class="form-control input-sm input-micro" type="text" value="1">
+								</td>
+								<td class="price">$999.99</td>
+								<td class="actions">
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
+								</td>
+							</tr>
+							<tr>
+								<td class="image"><a href="page-product-details.html"><img src="img/product3.jpg" alt="Item Name"></a></td>
+								<td>
+									<div class="cart-item-title"><a href="page-product-details.html">LOREM IPSUM DOLOR</a></div>
+									<div class="feature color">
+									</div>
+									<div class="feature">Size: <b>XXL</b></div>
+								</td>
+								<td class="quantity">
+									<input class="form-control input-sm input-micro" type="text" value="1">
+								</td>
+								<td class="price">$999.99</td>
+								<td class="actions">
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-pencil"></i></a>
+									<a href="#" class="btn btn-xs btn-grey"><i class="glyphicon glyphicon-trash"></i></a>
+								</td>
+							</tr>
+						</table>
+						<!-- End Shopping Cart Items -->
+					</div>
+				</div>
+				<div class="row">
+					<!-- Promotion Code -->
+					<div class="col-md-4  col-md-offset-0 col-sm-6 col-sm-offset-6">
+						<div class="cart-promo-code">
+							<h6><i class="glyphicon glyphicon-gift"></i> Have a promotion code?</h6>
+							<div class="input-group">
+								<input class="form-control input-sm" id="appendedInputButton" type="text" value="">
+								<span class="input-group-btn">
+									<button class="btn btn-sm btn-grey" type="button">Apply</button>
+								</span>
+							</div>
+						</div>
+					</div>
+					<!-- Shipment Options -->
+					<div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-6">
+						<div class="cart-shippment-options">
+							<h6><i class="glyphicon glyphicon-plane"></i> Shippment options</h6>
+							<div class="input-append">
+								<select class="form-control input-sm">
+									<option value="1">Standard - FREE</option>
+									<option value="2">Next day delivery - $10.00</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					
+					<!-- Shopping Cart Totals -->
+					<div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-6">
+						<table class="cart-totals">
+							<tr>
+								<td><b>Shipping</b></td>
+								<td>Free</td>
+							</tr>
+							<tr>
+								<td><b>Discount</b></td>
+								<td>- $18.00</td>
+							</tr>
+							<tr class="cart-grand-total">
+								<td><b>Total</b></td>
+								<td><b>$163.55</b></td>
+							</tr>
+						</table>
+						<!-- Action Buttons -->
+						<div class="pull-right">
+							<a href="#" class="btn btn-grey"><i class="glyphicon glyphicon-refresh"></i> UPDATE</a>
+							<a href="#" class="btn"><i class="glyphicon glyphicon-shopping-cart icon-white"></i> CHECK OUT</a>
 						</div>
 					</div>
 				</div>
