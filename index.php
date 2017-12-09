2bcb42a6cbd772faf55d03535d4da55b4f8fd053
m3nkk <M3nKK@localhost>
2017-12-09 18:48:41 +0300

    Database integration

--- index.php
+++ index.php
@@ -131,129 +131,35 @@
         <div class="section">
 	    	<div class="container">
 				<div class="row">
-					<div class="col-sm-4">
-						<!-- Product -->
-						<div class="shop-item">
-							<!-- Product Image -->
-							<div class="image">
-								<a href="page-product-details.html"><img src="img/product1.jpg" alt="Item Name"></a>
-							</div>
-							<!-- Product Title -->
-							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
-							</div>
-							<!-- Product Price-->
-							<div class="price">
-								<span class="price-was">$959.99</span> $999.99
-							</div>
-							<!-- Product Description-->
-							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
-							</div>
-							<!-- Add to Cart Button -->
-							<div class="actions">
-								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
-							</div>
-						</div>
-						<!-- End Product -->
-					</div>
+					<?php
+						if ($result->num_rows > 0) {
+   							while($row = $result->fetch_assoc()) {
+   							    
+   					?>
 					<div class="col-sm-4">
 						<div class="shop-item">
 							<div class="image">
-								<a href="page-product-details.html"><img src="img/product2.jpg" alt="Item Name"></a>
+								<a href="page-product-details.html"><img src="<?php  echo $row["image_link"]?>" alt="Item Name"></a>
 							</div>
 							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
+								<h3><a href="page-product-details.html"><?php  echo $row["name"]?></a></h3>
 							</div>
 							<div class="price">
-								$999.99
+								$<?php  echo $row["price"]?>
 							</div>
 							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
+								<p><?php  echo $row["short_description"]?></p>
 							</div>
 							<div class="actions">
 								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
 							</div>
 						</div>
 					</div>
-					<div class="col-sm-4">
-						<div class="shop-item">
-							<div class="image">
-								<a href="page-product-details.html"><img src="img/product3.jpg" alt="Item Name"></a>
-							</div>
-							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
-							</div>
-							<div class="price">
-								$999.99
-							</div>
-							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
-							</div>
-							<div class="actions">
-								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4">
-						<div class="shop-item">
-							<div class="image">
-								<a href="page-product-details.html"><img src="img/product4.jpg" alt="Item Name"></a>
-							</div>
-							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
-							</div>
-							<div class="price">
-								$999.99
-							</div>
-							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
-							</div>
-							<div class="actions">
-								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="shop-item">
-							<div class="image">
-								<a href="page-product-details.html"><img src="img/product5.jpg" alt="Item Name"></a>
-							</div>
-							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
-							</div>
-							<div class="price">
-								$999.99
-							</div>
-							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
-							</div>
-							<div class="actions">
-								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="shop-item">
-							<div class="image">
-								<a href="page-product-details.html"><img src="img/product6.jpg" alt="Item Name"></a>
-							</div>
-							<div class="title">
-								<h3><a href="page-product-details.html">Lorem ipsum dolor</a></h3>
-							</div>
-							<div class="price">
-								$999.99
-							</div>
-							<div class="description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio.</p>
-							</div>
-							<div class="actions">
-								<a href="page-product-details.html" class="btn"><i class="icon-shopping-cart icon-white"></i> Add to Cart</a>
-							</div>
-						</div>
-					</div>
+					
+					<?php 
+   							}
+						}
+					?>
 				</div>
 				<div class="pagination-wrapper ">
 					<ul class="pagination pagination-lg">
